98c0fb10c037922abc985e14473f034b0be67144
Beanking77 <beanking77@gmail.com>
2016-02-22 10:50:46 +0800

    Update updater.py

--- updater.py
+++ updater.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         for i in range(obj['total_count']):
             issue_id=obj['issues'][i]['id']
             version_id=sys.argv[1]
-            os.system('curl -L -k -v -H \"Content-Type: application/json\" -X PUT -d \'{ \"issue\": { \"status_id\": \"$issue_status_id\", \"assigned_to_id\": \"$assign_to_id\", \"fixed_version_id\": \"%s\"} }\' -H \"X-Redmine-API-Key: $redmine_api_key\" $redmine_project_issue_url/%s.json' % (version_id, issue_id))
+            os.system('curl -L -k -v -H \"Content-Type: application/json\" -X PUT -d \'{ \"issue\": { \"status_id\": \"%s\", \"assigned_to_id\": \"%s\", \"fixed_version_id\": \"%s\"} }\' -H \"X-Redmine-API-Key: %s\" %s/%s.json' % (issue_status_id, assign_to_id, version_id, redmine_api_key, redmine_project_issue_url, issue_id))
